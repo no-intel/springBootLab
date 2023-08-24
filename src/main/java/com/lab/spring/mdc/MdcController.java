2752fdacb60b45134cd4c0c697bbdbfb0c80718f
no-int <tkdduf212@gmail.com>
2023-08-25 02:44:44 +0900

    mdc

---
+++ src/main/java/com/lab/spring/mdc/MdcController.java
@@ -0,0 +1,21 @@
+package com.lab.spring.mdc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MdcController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    @GetMapping("/")
+    public void test(){
+        log.info("테스트 시작");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("테스트 끝");
+    }
+}
