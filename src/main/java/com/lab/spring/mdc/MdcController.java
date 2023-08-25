903e043a39750a688f4b1fd1d148f7dc9f66843a
no-int <tkdduf212@gmail.com>
2023-08-25 19:44:54 +0900

    update

--- src/main/java/com/lab/spring/mdc/MdcController.java
+++ src/main/java/com/lab/spring/mdc/MdcController.java
@@ -3,9 +3,11 @@ package com.lab.spring.mdc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/mdc")
 public class MdcController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     @GetMapping("/")
